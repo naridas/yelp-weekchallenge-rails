5f50da4a530872cc7f5feb1022f5962c9f5d7918
naridas <gregsparta@gmail.com>
2016-05-24 13:27:59 +0100

    1-10

--- yelp_clone/spec/features/restaurants_feature_spec.rb
+++ yelp_clone/spec/features/restaurants_feature_spec.rb
@@ -10,9 +10,8 @@ feature 'restaurants' do
   end
 
   context 'restaurants have been added' do
-    before do
-      Restaurant.create(name: 'KFC')
-    end
+
+    before { Restaurant.create name: 'KFC', description: 'Deep fried goodness' }
 
     scenario 'display restaurants' do
       visit '/restaurants'
@@ -29,6 +28,34 @@ feature 'restaurants' do
       click_button 'Create Restaurant'
       expect(page).to have_content 'KFC'
       expect(current_path).to eq '/restaurants'
+    end
+  end
+
+  context 'viewing restaurants' do
+
+    let!(:kfc){ Restaurant.create(name:'KFC', description: 'Deep fried goodness' ) }
+
+    scenario 'lets a user view a restaurant' do
+     visit '/restaurants'
+     click_link 'KFC'
+     expect(page).to have_content 'KFC'
+     expect(current_path).to eq "/restaurants/#{kfc.id}"
+    end
+  end
+
+  context 'editing restaurants' do
+
+    before { Restaurant.create name: 'KFC', description: 'Deep fried goodness' }
+
+    scenario 'let a user edit a restaurant' do
+     visit '/restaurants'
+     click_link 'Edit KFC'
+     fill_in 'Name', with:'Kentucky Fried Chicken'
+     fill_in 'Description', with:'Deep fried goodness'
+     click_button 'Update Restaurant'
+     expect(page).to have_content 'Kentucky Fried Chicken'
+     expect(page).to have_content 'Deep fried goodness'
+     expect(current_path).to eq '/restaurants'
     end
   end
 end
