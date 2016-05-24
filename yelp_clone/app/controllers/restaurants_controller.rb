0ebb0e7f62b922f5e57f0902237538bfa5986eb9
naridas <gregsparta@gmail.com>
2016-05-24 12:16:23 +0100

    1-8 complete

--- yelp_clone/app/controllers/restaurants_controller.rb
+++ yelp_clone/app/controllers/restaurants_controller.rb
@@ -1,5 +1,20 @@
 class RestaurantsController < ApplicationController
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.create(restaurant_params)
+    redirect_to '/restaurants'
+  end
+
   def index
     @restaurants = Restaurant.all
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name)
   end
 end
