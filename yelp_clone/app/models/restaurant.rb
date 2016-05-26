7159dbb3f16bb4276280e8d8362451124e623550
naridas <gregsparta@gmail.com>
2016-05-26 18:15:49 +0100

    Max Pizza startup

--- yelp_clone/app/models/restaurant.rb
+++ yelp_clone/app/models/restaurant.rb
@@ -3,6 +3,9 @@ class Restaurant < ActiveRecord::Base
     -> { extending WithUserAssociationExtension },
     dependent: :destroy
 
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
   belongs_to :user
   validates :name, length: { minimum: 3 }, uniqueness: true
   # validates_associated :reviews
