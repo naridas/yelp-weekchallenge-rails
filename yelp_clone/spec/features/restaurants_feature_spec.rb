7b6d9b3b853c0b807da83f7917792ca57242991c
naridas <gregsparta@gmail.com>
2016-05-26 12:05:00 +0100

    3-1 complete working on 3-2

--- yelp_clone/spec/features/restaurants_feature_spec.rb
+++ yelp_clone/spec/features/restaurants_feature_spec.rb
@@ -21,7 +21,13 @@ feature 'restaurants' do
   end
 
   context 'creating restaurants' do
+    scenario 'You have to sign in order to create,edit and delete a restaurant' do
+      visit '/restaurants'
+      click_link 'Add a restaurant'
+      expect(page).to have_content 'You need to sign in or sign up before continuing.'
+    end
     scenario 'prompts user to fill out a form, then displays the new restaurant' do
+      signing_up
       visit '/restaurants'
       click_link 'Add a restaurant'
       fill_in 'Name', with: 'KFC'
@@ -48,20 +54,22 @@ feature 'restaurants' do
     before { Restaurant.create name: 'KFC', description: 'Deep fried goodness' }
 
     scenario 'let a user edit a restaurant' do
-     visit '/restaurants'
-     click_link 'Edit KFC'
-     fill_in 'Name', with:'Kentucky Fried Chicken'
-     fill_in 'Description', with:'Deep fried goodness'
-     click_button 'Update Restaurant'
-     expect(page).to have_content 'Kentucky Fried Chicken'
-     expect(page).to have_content 'Deep fried goodness'
-     expect(current_path).to eq '/restaurants'
+      signing_up
+      visit '/restaurants'
+      click_link 'Edit KFC'
+      fill_in 'Name', with:'Kentucky Fried Chicken'
+      fill_in 'Description', with:'Deep fried goodness'
+      click_button 'Update Restaurant'
+      expect(page).to have_content 'Kentucky Fried Chicken'
+      expect(page).to have_content 'Deep fried goodness'
+      expect(current_path).to eq '/restaurants'
     end
   end
 
   context 'deleting restaurants' do
     before { Restaurant.create name: 'KFC', description: 'Deep fried goodness' }
     scenario 'removes a restaurant when a user clicks a delete link' do
+      signing_up
       visit '/restaurants'
       click_link 'Delete KFC'
       expect(page).not_to have_content 'KFC'
@@ -71,6 +79,7 @@ feature 'restaurants' do
 
    context 'an invalid restaurant' do
     it 'does not let you submit a name that is too short' do
+      signing_up
       visit '/restaurants'
       click_link 'Add a restaurant'
       fill_in 'Name', with: 'kf'
